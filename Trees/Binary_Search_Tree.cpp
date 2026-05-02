4fe25bbcbdc9125220a1f2c2837d71086fe14fa6
Aveeral <aveeraljain22@gmail.com>
2026-05-02 20:19:28 +0530

    Deletion of Node in BST

--- Trees/Binary_Search_Tree.cpp
+++ Trees/Binary_Search_Tree.cpp
@@ -6,94 +6,111 @@ public:
     int data;
     Node* left;
     Node* right;
-
-    Node(int value)
-    {
+    Node(int value) {
         data = value;
         left = NULL;
         right = NULL;
     }
 };
 
-// INSERTION IN BINARY TREE
-Node* createBST(Node* root, int value)
-{
-    // Base case
-    if(root == NULL)
-    {
+// INSERTION IN BST
+Node* createBST(Node* root, int value) {
+    if(root == NULL) {
         Node* newNode = new Node(value);
         return newNode;
     }
-
-    // Insert in left subtree
-    if(value < root->data)
-    {
+    if(value < root->data) {
         root->left = createBST(root->left, value);
-    }
-
-    // Insert in right subtree
-    else if(value > root->data)
-    {
+    } else if(value > root->data) {
         root->right = createBST(root->right, value);
     }
-
     return root;
 }
 
-void inorder(Node* root)
-{
-    if(root == NULL)
-        return;
-
+// INORDER TRAVERSAL
+void inorder(Node* root) {
+    if(root == NULL) return;
     inorder(root->left);
     cout << root->data << " ";
     inorder(root->right);
 }
 
 // SEARCHING IN BST
+bool Search(Node* root, int key) {
+    if(root == NULL) return false;
+    if(root->data == key) return true;
+    else if(key < root->data) return Search(root->left, key);
+    else return Search(root->right, key);
+}
 
-  bool Search(Node*root,int key){
-    if(root == NULL){
-        return false;
+// DELETION IN BST
+int Min_value(Node* root) {
+    Node* temp = root;
+    while(temp->left != NULL) {
+        temp = temp->left;
     }
+    return temp->data;
+}
 
-    if(root->data == key){
-        return true;
-    }
-    else if(key<root->data){
-        return Search(root->left,key);
-    }
-    else{
-        return Search(root->right,key);
-    }
-    
-  }
+Node* deleteNode(Node* root, int key) {
+    if(root == NULL) return NULL;
 
-int main()
-{
+    if(key < root->data) {
+        root->left = deleteNode(root->left, key);
+    } else if(key > root->data) {
+        root->right = deleteNode(root->right, key);
+    } else {
+        // Case 1: No child
+        if(root->left == NULL && root->right == NULL) {
+            delete root;
+            return NULL;
+        }
+        // Case 2: One child
+        else if(root->left == NULL) {
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        } else if(root->right == NULL) {
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+        // Case 3: Two children
+        else {
+            int minVal = Min_value(root->right);
+            root->data = minVal;
+            root->right = deleteNode(root->right, minVal);
+        }
+    }
+    return root;
+}
+
+int main() {
     Node* root = NULL;
-
     int value;
 
-    while(true)
-    {
+    while(true) {
         cout << "ENTER VALUE (-1 TO STOP): ";
         cin >> value;
-
-        if(value == -1)
-            break;
-
+        if(value == -1) break;
         root = createBST(root, value);
     }
 
-    cout << "Searching a key\n";
-    int key = -1;
-    cout<< "ENTER THE VALUE OF KEY: ";
-    cin >> key;
-    cout << Search(root,key);
-
-
     cout << "\nINORDER TRAVERSAL: ";
+    inorder(root);
+
+    cout << "\n\nSearching a key";
+    int key = -1;
+    cout << "\nENTER THE VALUE OF KEY: ";
+    cin >> key;
+    cout << "FOUND: " << Search(root, key);
+
+    cout << "\n\nDELETION";
+    int delKey;
+    cout << "\nENTER THE VALUE TO DELETE: ";
+    cin >> delKey;
+    root = deleteNode(root, delKey);
+    cout << "INORDER AFTER DELETION: ";
     inorder(root);
 
     return 0;
